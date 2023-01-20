1b001c4c50ff79d3a523cac8bf29561c4611ca87
Will Li <willeli2002@gmail.com>
2023-01-19 23:59:02 -0800

    initial commit

---
+++ src/components/products/index.jsx
@@ -0,0 +1,181 @@
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import { useGlobalFilter, useSortBy, useTable } from "react-table";
+import tw from "twin.macro";
+import { GlobalFilter } from "./globalFilter";
+
+import gene_data from "./MAGMA_Gene_Level_Analysis_Results_12_01_22_Modified.xlsx"
+
+const XLXS = require("xlsx")
+
+const Table = tw.table`
+  table-fixed
+  text-base
+  text-gray-900
+`;
+
+const TableHead = tw.thead`
+  p-2
+`;
+
+const TableRow = tw.tr`
+border
+border-green-500
+`;
+
+const TableHeader = tw.th`
+border
+border-green-500
+p-2
+`;
+
+const TableBody = tw.tbody`
+
+`;
+
+const TableData = tw.td`
+border
+border-green-500
+p-5
+`;
+
+const Button = tw.button`
+  pl-4
+  pr-4
+  pt-2
+  pb-2
+  text-black
+  rounded-md
+  bg-green-300
+  hover:bg-green-200
+  transition-colors
+`;
+
+export function Products(props) {
+  const [products, setProducts] = useState([]);
+
+  // TODO: add API, rename, add more genes
+  /*
+  const wb = XLXS.readFile("MAGMA_Gene_Level_Analysis_Results_12_01_22_Modified.xlsx");
+  const SCZ_sheet = wb.Sheets["SCZ_2022"];
+  const scz_json = XLXS.utils.sheet_to_json(SCZ_sheet);
+  console.log(scz_json)
+  */
+
+  console.log(gene_data);
+
+  // let fileHandle;
+  const fetchProducts = async () => {
+    // const file = await fileHandle.getFile("MAGMA_Gene_Level_Analysis_Results_12_01_22_Modified.xlsx");
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => console.log(err));
+
+    if (response) {
+      const products = response.data;
+
+      console.log("Products: ", products);
+      setProducts(products);
+    }
+  };
+
+  const productsData = useMemo(() => [...products], [products]);
+
+  const productsColumns = useMemo(
+    () =>
+      products[0]
+        ? Object.keys(products[0])
+            .filter((key) => key !== "rating")
+            .map((key) => {
+              return { Header: key, accessor: key };
+            })
+        : [],
+    [products]
+  );
+
+  const tableHooks = (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      ...columns,
+      {
+        id: "Edit",
+        Header: "Edit",
+        Cell: ({ row }) => (
+          <Button onClick={() => alert("Editing: " + row.values.price)}>
+            Edit
+          </Button>
+        ),
+      },
+    ]);
+  };
+
+  const tableInstance = useTable(
+    {
+      columns: productsColumns,
+      data: productsData,
+    },
+    useGlobalFilter,
+    // tableHooks,
+    useSortBy
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    state,
+  } = tableInstance;
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const isEven = (idx) => idx % 2 === 0;
+
+  return (
+    <>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        setGlobalFilter={setGlobalFilter}
+        globalFilter={state.globalFilter}
+      />
+      <Table {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableHeader
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  {column.render("Header")}
+                  {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
+                </TableHeader>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {rows.map((row, idx) => {
+            prepareRow(row);
+
+            return (
+              <TableRow
+                {...row.getRowProps()}
+                className={isEven(idx) ? "bg-green-400 bg-opacity-30" : ""}
+              >
+                {row.cells.map((cell, idx) => (
+                  <TableData {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </TableData>
+                ))}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
